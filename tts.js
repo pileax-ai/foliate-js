66207908b53a9c6ad23673f9ee15c428814cbf92
pileax <ipileax@gmail.com>
2025-12-02 14:02:10 +0800

    feat: 🎸 hight speak range in tts.from()

--- tts.js
+++ tts.js
@@ -256,16 +256,23 @@ export class TTS {
         if (paused && range) this.highlight(range.cloneRange())
         return this.#speak(doc)
     }
+    // TODO: EBOOK (highlight speak range)
     from(range) {
         this.#lastMark = null
-        const [doc] = this.#list.find(range_ =>
+        const [doc, r] = this.#list.find(range_ =>
             range.compareBoundaryPoints(Range.END_TO_START, range_) <= 0)
+        const speakRange = doc.createRange()
+        speakRange.setEnd(r.endContainer, r.endOffset)
+
         let mark
         for (const [name, range_] of this.#ranges.entries())
             if (range.compareBoundaryPoints(Range.START_TO_START, range_) <= 0) {
                 mark = name
+                speakRange.setStart(range_.startContainer, range_.startOffset)
                 break
             }
+
+        this.highlight(speakRange.cloneRange())
         return this.#speak(doc, ssml => this.#getMarkElement(ssml, mark))
     }
     setMark(mark) {
